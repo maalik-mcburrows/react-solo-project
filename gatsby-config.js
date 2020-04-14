398ceb38150cad43e88aa9afe71ebe7bf569d7d0
Maalik McBurrows <mmcburrows1@gmail.com>
2020-04-14 12:01:57 -0400

    gatsby starter blog, getting errors:  ERROR #85901  GRAPHQL
    There was an error in your GraphQL query:
    
    Expected type MarkdownRemarkFieldsEnum, found frontmatter__date. Did you mean the enum value frontmatter___data, frontmatter___path, or frontmatter___title?
    
    File: gatsby-node.js:15:26
     ERROR
    
    Error while running GraphQL query.
    
    failed createPages - 0.091s
    success createPagesStatefully - 0.116s
    success onPreExtractQueries - 0.003s
    success update schema - 0.045s
    
     ERROR #85911  GRAPHQL
    
    There was a problem parsing /Users/mymac/Documents/Solo-React/personal-blog/src/templates/blog-post.js; any GraphQL
    fragments or queries in this file were not processed.
    
    This may indicate a syntax error in the code, or it may be a file type
    that Gatsby does not know how to parse.
    
    File: src/templates/blog-post.js
     ERROR
    
    Error while running GraphQL query.
    
    failed createPages - 0.091s
    success createPagesStatefully - 0.116s
    success onPreExtractQueries - 0.003s
    success update schema - 0.045s
    
     ERROR #85911  GRAPHQL
    
    There was a problem parsing /Users/mymac/Documents/Solo-React/personal-blog/src/templates/blog-post.js; any GraphQL
    fragments or queries in this file were not processed.
    
    This may indicate a syntax error in the code, or it may be a file type
    that Gatsby does not know how to parse.
    
    File: src/templates/blog-post.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,21 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-catch-links', 
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
