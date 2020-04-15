1945bc05f8c3400bd4ce49566f0c85f89ea5f29c
Maalik McBurrows <mmcburrows1@gmail.com>
2020-04-15 09:45:15 -0400

    trying to solve ref passing issue (from app to artistList component)

--- V3/src/components/ArtistContent.jsx
+++ V3/src/components/ArtistContent.jsx
@@ -4,11 +4,7 @@ const ArtistContent = props => {
     const { data } = props;
 
     return(
-        <div style={`background-image: url(${data.artworkUrl100})`} onClick={`openMedia('${data.previewUrl}', '${data.trackCencoredName}')`} className='result'>
-            {/* <img src={data} alt="User Pic" />
-            <br></br>
-            <a href={`/artist/${data}`} target="_blank" rel="noopener noreferrer" key={data.id}>{data}</a> */}
-        </div>
+        <div style={`background-image: url(${data.artworkUrl100})`} onClick={`openMedia('${data.previewUrl}', '${data.trackCencoredName}')`} className='result'></div>
     );
 };
 
